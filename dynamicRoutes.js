0d7eac1f45f64a268b248932ce9eb8705469dab0
giffen-good <chris6rock@gmail.com>
2021-11-26 12:40:35 -0500

    first commit

--- dynamicRoutes.js
+++ dynamicRoutes.js
@@ -13,19 +13,4 @@ const dynamicRoutes = () => {
       return res.data.map((post) => `/insights/${post.slug}`)
     })
 }
-
-// const getPageURLs = () => {
-//   return axios
-//     .get(`${wpConfig.wpDomain}${wpConfig.api.getPages}`, null, {
-//       params: {
-//         per_page: 100,
-//         page: 1,
-//       },
-//     })
-//     .then((res) => {
-//       return res.data.map((post) => `/${post.slug}`)
-//     })
-// }
-// console.log(getPageURLs())
-// const dynamicRoutes = [...getPostsURLs(), ...getPageURLs()]
 export default dynamicRoutes
